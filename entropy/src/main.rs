b6fe5c09728da808f45d4a53f4cafd0bb3c96793
francisdbillones <francisd.billones@gmail.com>
2023-06-10 09:45:28 +0800

    use ndarray and array computations instead of raw loops

--- entropy/src/main.rs
+++ entropy/src/main.rs
@@ -1,6 +1,8 @@
+use ndarray::{s, Array2};
 use pixel_canvas::{Canvas, Color};
 use rand::{rngs::ThreadRng, Rng};
 use serde::{Deserialize, Serialize};
+use std::iter::zip;
 use std::str;
 use std::{fs::File, io::BufReader};
 
@@ -24,19 +26,22 @@ fn start_loop(config: Config) {
     let (h, w) = config.dims;
 
     let mut lagged_board = init_board(&config);
-    let mut board = vec![vec![0; w]; h];
+    let mut board = Array2::zeros((h, w));
 
     let mut rng = rand::thread_rng();
 
     let canvas = Canvas::new(w * config.size_factor, h * config.size_factor);
+    let mut i = 0_usize;
 
     canvas.render(move |_, image| {
+        i += 1;
+        println!("{}", i);
         board_time_step(&mut board, &mut lagged_board, &config, &mut rng);
 
         for (y, row) in image.chunks_mut(w * config.size_factor).enumerate() {
             for (x, pixel) in row.iter_mut().enumerate() {
-                let energy = lagged_board[y / config.size_factor][x / config.size_factor];
-                let rgb = energy_to_rgb(energy, 2);
+                let energy = lagged_board[[y / config.size_factor, x / config.size_factor]];
+                let rgb = energy_to_rgb(energy, 2.0);
                 *pixel = rgb;
             }
         }
@@ -48,85 +53,70 @@ fn start_loop(config: Config) {
 
 #[inline(always)]
 fn board_time_step(
-    board: &mut [Vec<usize>],
-    lagged_board: &mut [Vec<usize>],
+    board: &mut Array2<f64>,
+    lagged_board: &mut Array2<f64>,
     config: &Config,
     rng: &mut ThreadRng,
 ) {
     let (h, w) = config.dims;
-    for (i, row) in lagged_board.iter().enumerate() {
-        for (j, &energy) in row.iter().enumerate() {
-            random_steps(board, (i, j), energy, config, rng);
-        }
+
+    let corner_slices = [
+        s![0..2_usize, 0..2_usize], // top left
+        s![0..2_usize, w - 2..w],   // top right
+        s![h - 2..h, 0..2_usize],   // bottom left
+        s![h - 2..h, w - 2..w],     // bottom right
+    ];
+    let corner_energies: [f64; 4] = [
+        lagged_board[[0, 0]],
+        lagged_board[[0, w - 1]],
+        lagged_board[[h - 1, 0]],
+        lagged_board[[h - 1, w - 1]],
+    ];
+
+    for (slice, energy) in zip(corner_slices, corner_energies) {
+        let mut slice = board.slice_mut(slice);
+        slice += &(energy * &probability_mat((2, 2), rng));
     }
 
-    for i in 0..h {
-        for j in 0..w {
-            lagged_board[i][j] = board[i][j];
-        }
+    // top and bottom borders
+    for j in 1..w - 1 {
+        let mut slice = board.slice_mut(s![0..2_usize, j - 1..=j + 1]);
+        let energy = lagged_board[[0, j]];
+        slice += &(energy * &probability_mat((2, 3), rng));
+    }
+    for j in 1..w - 1 {
+        let mut slice = board.slice_mut(s![h - 2..h, j - 1..=j + 1]);
+        let energy = lagged_board[[h - 1, j]];
+        slice += &(energy * &probability_mat((2, 3), rng));
     }
 
-    for i in 0..h {
-        for j in 0..w {
-            board[i][j] = 0;
+    // left to right
+    for i in 1..h - 1 {
+        // leftmost
+        let mut slice = board.slice_mut(s![i - 1..=i + 1, 0..2_usize]);
+        let energy = lagged_board[[i, 0]];
+        slice += &(energy * &probability_mat((3, 2), rng));
+
+        // in between
+        for j in 1..w - 1 {
+            let mut slice = board.slice_mut(s![i - 1..=i + 1, j - 1..=j + 1]);
+            let energy = lagged_board[[i, j]];
+            slice += &(energy * &probability_mat((3, 3), rng));
         }
+
+        // rightmost
+        let mut slice = board.slice_mut(s![i - 1..=i + 1, h - 2..h]);
+        let energy = lagged_board[[i, w - 1]];
+        slice += &(energy * &probability_mat((3, 2), rng));
     }
+
+    lagged_board.clone_from(board);
+
+    board.fill(0.0);
 }
 
 #[inline(always)]
-fn random_steps(
-    board: &mut [Vec<usize>],
-    (i, j): (usize, usize),
-    energy: usize,
-    config: &Config,
-    rng: &mut ThreadRng,
-) {
-    if rng.gen::<f64>() > config.heat {
-        return;
-    }
-
-    let (count, neighbours) = iterate_neighbours((i, j), config);
-
-    let percentages = gen_percentages(count, rng);
-
-    let mut s = 0;
-    for (cur_percentage, (ci, cj)) in neighbours.iter().take(count).enumerate() {
-        let dispersed_energy = (energy as f64 * percentages[cur_percentage]).floor() as usize;
-        board[*ci][*cj] += dispersed_energy;
-        s += dispersed_energy;
-    }
-    let (ri, rj) = neighbours[rng.gen_range(0..count)];
-    board[ri][rj] += energy - s;
-}
-
-#[inline(always)]
-fn iterate_neighbours((i, j): (usize, usize), config: &Config) -> (usize, [(usize, usize); 9]) {
-    let (h, w) = config.dims;
-    let (i, j) = (i as isize, j as isize);
-
-    let mut neighbours = [(0_usize, 0_usize); 9];
-    let mut count = 0;
-
-    for i in i - 1..=i + 1 {
-        if i < 0 || i >= h as isize {
-            continue;
-        }
-        for j in j - 1..=j + 1 {
-            if j < 0 || j >= w as isize {
-                continue;
-            }
-            neighbours[{
-                count += 1;
-                count - 1
-            }] = (i as usize, j as usize);
-        }
-    }
-
-    (count, neighbours)
-}
-
-#[inline(always)]
-fn energy_to_rgb(energy: usize, max_energy: usize) -> Color {
+fn energy_to_rgb(energy: f64, max_energy: f64) -> Color {
     let (energy, max_energy) = (energy as f64, max_energy as f64);
     let min_hue: f64 = 240.0; // Blue
     let max_hue: f64 = 0.0; // Red
@@ -166,48 +156,48 @@ fn hsv_to_rgb(h: f64, s: f64, v: f64) -> (f64, f64, f64) {
 }
 
 #[inline(always)]
-fn gen_percentages(n: usize, rng: &mut ThreadRng) -> [f64; 9] {
-    assert!(n <= 9);
-
-    let mut p: [f64; 9] = [0.0; 9];
-
+fn probability_mat((a, b): (usize, usize), rng: &mut ThreadRng) -> Array2<f64> {
+    let mut p = Array2::<f64>::zeros((a, b));
     let mut s = 0.0;
-    for v in p.iter_mut().take(n) {
-        let r = rng.gen();
-        *v = r;
-        s += r;
+
+    for i in 0..a {
+        for j in 0..b {
+            p[[i, j]] = rng.gen();
+            s += p[[i, j]];
+        }
     }
 
-    for v in p.iter_mut().take(n) {
-        *v /= s;
-    }
+    p /= s;
 
     p
 }
 
-fn init_board(config: &Config) -> Vec<Vec<usize>> {
+fn init_board(config: &Config) -> Array2<f64> {
     let (h, w) = config.dims;
     let hotspots = config.hotspots;
 
-    let mut board = vec![vec![0_usize; w]; h];
+    let mut board = Array2::<f64>::zeros((h, w));
 
     let mut rng = rand::thread_rng();
 
     let mut quota = 0;
 
-    loop {
-        if quota == hotspots {
-            break;
-        }
+    // pad board with negative infinities in its borders
+    // board.slice_mut(s![0, 0..w + 2]).fill(-f64::INFINITY);
+    // board.slice_mut(s![h + 1, 0..w + 2]).fill(-f64::INFINITY);
+    // board.slice_mut(s![0..h + 2, 0]).fill(-f64::INFINITY);
+    // board.slice_mut(s![0..h + 2, w + 1]).fill(-f64::INFINITY);
 
+    while quota != hotspots {
         let rx = rng.gen_range(0..w);
         let ry = rng.gen_range(0..h);
 
-        if board[ry][rx] != 0 {
+        if board[[ry, rx]] != 0.0 {
             continue;
         }
 
-        board[ry][rx] = (h * w * w / h) / hotspots;
+        let (h, w, hotspots) = (h as f64, w as f64, hotspots as f64);
+        board[[ry, rx]] = (h * w * w / h) / hotspots;
         quota += 1;
     }
 
